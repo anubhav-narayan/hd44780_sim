59d8965addf6f04c50550f34aed57cdd8bbfc2d6
Anubhav Mattoo <anubhavmattoo@outlook.com>
2025-04-27 15:50:06 +0530

    Add `LICENSE` and `README`

--- hd44780/hd44780.py
+++ hd44780/hd44780.py
@@ -1,9 +1,27 @@
+"""
+Software model of the Hitachi HD44780 LCD Controller.
+Copyright (C) 2025  Anubhav Mattoo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import pygame
 import threading
 
 class HD44780():
     """
-    Software model of the HD44780 LCD Controller.
+    Software model of the Hitachi HD44780 LCD Controller.
 
     This class emulates the behavior of the Hitachi HD44780 LCD controller,
     widely used in alphanumeric displays. It supports partial command set handling,
--- hd44780/hd44780_comps.py
+++ hd44780/hd44780_comps.py
@@ -1,3 +1,21 @@
+"""
+CoCoTB components for the Software model of the Hitachi HD44780 LCD Controller.
+Copyright (C) 2025  Anubhav Mattoo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import cocotb
 from cocotb_bus.bus import Bus
 
